e353ba87800d432a8c42aec7d297b5041ab16669
Dean Howe <hideanhowe@gmail.com>
2026-01-14 07:47:54 +0000

    testing

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 
 // Treat consecutive digits as a single number. For example, "13" counts as 13, not 1 + 3.
 // Ignore any non-digit characters.
+
+function stringSum(str) {
+  console.log(str);
+  // find the digits
+  const regex = /([\d]*)/g;
+  const arr = str.matchAll(str);
+  console.log(arr);
+
+  return str;
+}
